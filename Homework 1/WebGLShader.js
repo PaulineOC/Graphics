887e6d87942945c43b12ac559429b169fd445c5d
PaulineOC <poc215@nyu.edu>
2017-01-28 18:09:03 -0500

    moving sun

--- Homework 1/WebGLShader.js
+++ Homework 1/WebGLShader.js
@@ -1,10 +1,8 @@
 
-
 // THIS IS THE FRAGMENT SHADER WE CREATED IN CLASS.
 
 varying vec3 vPos;                               // Pixel position
 uniform float uTime;                             // Time
-vec2 A[3];
 
 float D(vec2 p) {          // Unit sphere: return z value.
    float rr = dot(p, p);
@@ -13,23 +11,26 @@ float D(vec2 p) {          // Unit sphere: return z value.
 
 void main() {
 
-   A[0] = vec2(.6, .2);
-   A[1] = vec2(-.3, .3);
-   A[2] = vec2(.4, .5);
 
    float x = vPos.x;
    float y = vPos.y;
-   vec2 p = vPos.xy;
-
+  
+vec2 p = vPos.xy;
    vec3 c = mix(vec3(1., .5, 0.), vec3(0.,.5,1.), .5 + .5 * y);//colors
 
 
-   float z = D(2. * p + vec2(1, .1 * sin(uTime * 2.)));//need to make ball smaller
+   float z = D(7. * p + vec2(-6.5 * cos(uTime * 1.), 5. * sin(uTime * 1.)));//sin wave animation
 
-   for (int i = 0 ; i < 3 ; i++) {
-      if (z > 0.)
-         c = vec3(z, z, z);
-   }
+
+
+
+   if (z > 0.)// needed to set sun color only
+     c = vec3(50, 50, 0);
+
+
+   if (y < 0.)
+     c = vec3(0,0,0);
+
 
    gl_FragColor = vec4(sqrt(c), 1.);             // Final pixel color
 }
