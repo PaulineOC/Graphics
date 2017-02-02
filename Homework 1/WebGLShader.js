6220d83c83be25b6743875ef5383aea50770507d
PaulineOC <poc215@nyu.edu>
2017-02-01 20:35:29 -0500

    sunrise/sunset/darkness

--- Homework 1/WebGLShader.js
+++ Homework 1/WebGLShader.js
@@ -12,7 +12,9 @@ float D(vec2 p) {          // Unit sphere: return z value.
 void main() {
   float x = vPos.x;
   float y = vPos.y;
-  vec2 p = vPos.xy; 
+  vec2 p = vPos.xy;
+
+ 
 float sunSpeed=1.;
 
 //Shapes
@@ -24,14 +26,71 @@ float sunSpeed=1.;
 
 //Colors section
 
-  vec3 c = mix(vec3(5., .5, 0.), vec3(0.,.5,1.), .5 + .5 * y);//original color gradient
+float r1=1.;
+float g1=.2;
+float b1=0.;
 
+float r2 =0.;
+float g2=.5;
+float b2=1.5;
+
+float saturation = 1.8;
+//saturation gets closer to 1 == more blue/less red --> more afternoon time* 
+//saturation gets closer to 0 == more red/less blue --> more sunrise/sunset
+
+float sunRiseSet =0.1; //lowerlimit for saturation
+
+
+float actualGradient = 1.5;
+//when actualGradient approaches infinity = 
+//when actualGradient approaches 0. = 
+
+
+float horizonOffset =.7;
+
+float speedOfSunsetSunRise=1.;
+
+
+//this is the gradient from the middle that changes outwards
+float test = pow(y+horizonOffset,(.95+.95*sin(speedOfSunsetSunRise*uTime)));
+
+float t = 0.5+0.5*sin(uTime+3.14);
+
+  //vec3 c = mix(vec3(r1, g1, b1), vec3(r2,g2,b2), sunRiseSet + actualGradient * y);//original color gradient
+
+
+float earthColor = 0.5;
+
+if (-6.5 * sin(1. * uTime) < 0.) {
+//Oscillates each color between black and itself at its brightest overall
+//Multiply by the color (r1) to get the magnitude of the sin wave (eg: height)
+//Multiply by -1 to flip it
+//Add the color itself (eg: r1) to offset so that its lowest value is 0
+
+   r1 = r1 - sin(uTime * 1.) * r1;
+   r2 = r2 - sin(uTime * 1.) * r2;
+   b2 = b2 - sin(uTime * 1.) * b2;
+   b1 = b1 - sin(uTime * 1.) * b1;
+   g1 = g1 - sin(uTime * 1.) * g1;
+   g2 = g2 - sin(uTime * 1.) * g2;
+
+
+   earthColor = earthColor - sin(uTime * 1.) * earthColor * 0.9;
+}
+
+vec3 c = mix(vec3(r1,g1,b1),vec3(r2,g2,b2),test);
+
+//interpolant with y raised to a power 
+//wants on something 
+//whatever you add with sin function to add 
+//need it to be cyclic aka sin or cos 
+
+float exp=1.+0.5+sin(uTime);
 
   //changing colors of blue and green for sky with time 
   float b = 1. - 1. * sin(1. * uTime);
-  float g = 0.3 - 0.3 * sin(1. * uTime);
+  float g = 0.3 - 0.3 * sin(.85 * uTime);
   
-
 //comment this out below and get the gradient back
   //c = vec3(0,g,b);
 
@@ -41,11 +100,7 @@ float sunSpeed=1.;
    }
    //color the earth
    if (earth > 0.)
-     c = vec3(0., 0.5, 0);
-
-
-//gradient 
-
+     c = vec3(0., earthColor, 0);
 
    gl_FragColor = vec4(sqrt(c), 1.);             // Final pixel color
 }
